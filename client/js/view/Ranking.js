18d186c637614d891b125ea8738d9c2573daf889
Marcin Biłka <bilkadev@gmail.com>
2024-10-06 15:24:56 +0200

    update client

--- client/js/view/Ranking.js
+++ client/js/view/Ranking.js
@@ -1,61 +1,63 @@
-import { ItemInTable } from '../components/ItemInTable.js';
-import { getTopStats } from './../api/stat.js';
+import {ItemInTable} from '../components/ItemInTable.js';
+import {getTopStats} from './../api/stat.js';
 
 export class Ranking {
-	#htmlElements = {
-		container: document.querySelector('[data-modal-ranking]'),
-		buttonBack: document.querySelector('[data-button-ranking-back]'),
-		modalStart: document.querySelector('[data-modal-start-view]'),
-		modalEndGame: document.querySelector('[data-modal]'),
-		tbody: document.querySelector('[data-tbody-ranking]'),
-		errorSpan: document.querySelector('[data-error-ranking]'),
-	};
-	#isFirstGame = true;
-	#itemInTable = new ItemInTable(this.#htmlElements.tbody);
+    #htmlElements = {
+        container: document.querySelector('[data-modal-ranking]'),
+        buttonBack: document.querySelector('[data-button-ranking-back]'),
+        modalStart: document.querySelector('[data-modal-start-view]'),
+        modalEndGame: document.querySelector('[data-modal]'),
+        tbody: document.querySelector('[data-tbody-ranking]'),
+        errorSpan: document.querySelector('[data-error-ranking]'),
+    };
+    #isFirstGame = true;
+    #itemInTable = new ItemInTable(this.#htmlElements.tbody);
 
-	constructor() {
-		this.loadStats();
-		this.#htmlElements.buttonBack.addEventListener('click', () => {
-			this.#isFirstGame
-				? this.#htmlElements.modalStart.classList.remove('hide')
-				: this.#htmlElements.modalEndGame.classList.remove('hide');
-			this.hide();
-		});
-	}
+    constructor() {
+        this.loadStats();
+        this.#htmlElements.buttonBack.addEventListener('click', () => {
+            this.#isFirstGame
+                ? this.#htmlElements.modalStart.classList.remove('hide')
+                : this.#htmlElements.modalEndGame.classList.remove('hide');
+            this.hide();
+        });
+    }
 
-	#clearTable() {
-		this.#htmlElements.tbody.innerHTML = '';
-	}
+    #clearTable() {
+        this.#htmlElements.tbody.innerHTML = '';
+    }
 
-	#addError(message) {
-		this.#htmlElements.errorSpan.classList.remove('hide');
-		this.#htmlElements.errorSpan.textContent = message;
-	}
+    #addError(message) {
+        this.#htmlElements.errorSpan.classList.remove('hide');
+        this.#htmlElements.errorSpan.textContent = message;
+    }
 
-	#clearError() {
-		this.#htmlElements.errorSpan.textContent = '';
-		this.#htmlElements.errorSpan.classList.add('hide');
-	}
+    #clearError() {
+        this.#htmlElements.errorSpan.textContent = '';
+        this.#htmlElements.errorSpan.classList.add('hide');
+    }
 
-	loadStats() {
-		this.#clearError();
-		this.#clearTable();
-		getTopStats()
-			.then((res) => {
-				res.data.forEach((item, id) => {
-					this.#itemInTable.add({ rank: id + 1, username: item.username, score: item.score });
-				});
-			})
-			.catch((e) => this.#addError('Something went wrong, Please try later.'));
-	}
+    loadStats() {
+        this.#clearError();
+        this.#clearTable();
+        getTopStats()
+            .then((res) => {
+                res.data.forEach((item, id) => {
+                    this.#itemInTable.add({rank: id + 1, username: item.user.name, score: item.score});
+                });
+            })
+            .catch((e) => this.#addError('Something went wrong, Please try later.'));
+    }
 
-	setIsFirstGame() {
-		this.#isFirstGame = false;
-	}
-	hide() {
-		this.#htmlElements.container.classList.add('hide');
-	}
-	show() {
-		this.#htmlElements.container.classList.remove('hide');
-	}
+    setIsFirstGame() {
+        this.#isFirstGame = false;
+    }
+
+    hide() {
+        this.#htmlElements.container.classList.add('hide');
+    }
+
+    show() {
+        this.#htmlElements.container.classList.remove('hide');
+    }
 }
--- client/js/view/UserStats.js
+++ client/js/view/UserStats.js
@@ -1,63 +1,68 @@
-import { ItemInTable } from '../components/ItemInTable.js';
-import { getUserStats } from './../api/stat.js';
-import { loadFromStorage } from './../storage.js';
+import {ItemInTable} from '../components/ItemInTable.js';
+import {getUserStats} from './../api/stat.js';
+import {loadFromStorage} from './../storage.js';
 
 export class UserStats {
-	#htmlElements = {
-		container: document.querySelector('[data-modal-user-stats]'),
-		buttonBack: document.querySelector('[data-button-user-stats-back]'),
-		modalStart: document.querySelector('[data-modal-start-view]'),
-		modalEndGame: document.querySelector('[data-modal]'),
-		tbody: document.querySelector('[data-tbody-user-stats]'),
-		errorSpan: document.querySelector('[data-error-user-stats]'),
-	};
-	#isFirstGame = true;
-	constructor() {
-		this.#htmlElements.buttonBack.addEventListener('click', () => {
-			this.#isFirstGame
-				? this.#htmlElements.modalStart.classList.remove('hide')
-				: this.#htmlElements.modalEndGame.classList.remove('hide');
-			this.hide();
-		});
-	}
-	#itemInTable = new ItemInTable(this.#htmlElements.tbody);
+    #htmlElements = {
+        container: document.querySelector('[data-modal-user-stats]'),
+        buttonBack: document.querySelector('[data-button-user-stats-back]'),
+        modalStart: document.querySelector('[data-modal-start-view]'),
+        modalEndGame: document.querySelector('[data-modal]'),
+        tbody: document.querySelector('[data-tbody-user-stats]'),
+        errorSpan: document.querySelector('[data-error-user-stats]'),
+    };
+    #isFirstGame = true;
 
-	#clearTable() {
-		this.#htmlElements.tbody.innerHTML = '';
-	}
+    constructor() {
+        this.#htmlElements.buttonBack.addEventListener('click', () => {
+            this.#isFirstGame
+                ? this.#htmlElements.modalStart.classList.remove('hide')
+                : this.#htmlElements.modalEndGame.classList.remove('hide');
+            this.hide();
+        });
+    }
 
-	#addError(message) {
-		this.#htmlElements.errorSpan.classList.remove('hide');
-		this.#htmlElements.errorSpan.textContent = message;
-	}
+    #itemInTable = new ItemInTable(this.#htmlElements.tbody);
 
-	#clearError() {
-		this.#htmlElements.errorSpan.textContent = '';
-		this.#htmlElements.errorSpan.classList.add('hide');
-	}
+    #clearTable() {
+        this.#htmlElements.tbody.innerHTML = '';
+    }
 
-	loadStats() {
-		const user = loadFromStorage('user');
-		this.#clearError();
-		this.#clearTable();
+    #addError(message) {
+        this.#htmlElements.errorSpan.classList.remove('hide');
+        this.#htmlElements.errorSpan.textContent = message;
+    }
 
-		getUserStats(user.id)
-			.then((res) => {
-				res.data.forEach((item, id) => {
-					this.#itemInTable.add({ rank: id + 1, username: item.username, score: item.score }, false);
-				});
-			})
-			.catch((e) => {
-				this.#addError('Something went wrong, Please try later.');
-			});
-	}
-	setIsFirstGame() {
-		this.#isFirstGame = false;
-	}
-	hide() {
-		this.#htmlElements.container.classList.add('hide');
-	}
-	show() {
-		this.#htmlElements.container.classList.remove('hide');
-	}
+    #clearError() {
+        this.#htmlElements.errorSpan.textContent = '';
+        this.#htmlElements.errorSpan.classList.add('hide');
+    }
+
+    loadStats() {
+        const user = loadFromStorage('user');
+        this.#clearError();
+        this.#clearTable();
+
+        getUserStats(user.id)
+            .then((res) => {
+                res.data.forEach((item, id) => {
+                    this.#itemInTable.add({rank: id + 1, username: item.username, score: item.score}, false);
+                });
+            })
+            .catch((e) => {
+                this.#addError('Something went wrong, Please try later.');
+            });
+    }
+
+    setIsFirstGame() {
+        this.#isFirstGame = false;
+    }
+
+    hide() {
+        this.#htmlElements.container.classList.add('hide');
+    }
+
+    show() {
+        this.#htmlElements.container.classList.remove('hide');
+    }
 }
